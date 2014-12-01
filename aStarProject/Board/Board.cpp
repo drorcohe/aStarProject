9fc0350a34d662b5074aeb0b016622f9c6c250a6
drorcohe <drorcohe@intel.com>
2014-12-01 18:19:38 +0200

    first commit

---
+++ aStarProject/Board/Board.cpp
@@ -0,0 +1,18 @@
+
+
+
+#include "Board.h"
+
+int main(){
+	return 1;
+}
+
+
+void Board::init(std::string boardPath,std::string imagePath){}
+std::vector<Circle> Board::getBoard(){
+	return std::vector<Circle>();	
+}
+
+std::vector<int> Board::getSolution(){
+	return std::vector<int>();	
+}
---
+++ aStarProject/Board/Board.h
@@ -0,0 +1,35 @@
+#ifndef _BOARD_H
+#define BOARD_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+class Circle{
+
+public:
+	int x,y;
+	int radius;
+	std::vector<int> neighbours;
+	float R,G,B; //from 0 to 1
+};
+
+
+
+class Board{
+public:
+	void init(std::string boardPath,std::string imagePath);
+	std::vector<Circle> getBoard();	
+	int getStartCircle(){ return startCircle; };
+	int getEndCircle(){ return endCircle; };
+	std::vector<int> getSolution();
+
+private:
+	int startCircle,endCircle;
+	std::vector<Circle> board;
+	
+};
+
+
+#endif
