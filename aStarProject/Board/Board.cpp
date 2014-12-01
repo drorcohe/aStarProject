d34c43521f887514c82e73d5466c5bdcd1b6097c
drorcohe <drorcohe@intel.com>
2014-12-01 19:55:04 +0200

    implemented a star algorithm

--- aStarProject/Board/Board.cpp
+++ aStarProject/Board/Board.cpp
@@ -1,20 +1,119 @@
 
-
-
+#include <set>
+#include <math.h>
 #include "Board.h"
-
+#include <map>
+#include <queue>
+#include <functional>
+#include <assert.h>
 int main(){
 	return 1;
 }
 
 
 void Board::init(std::string boardPath,std::string imagePath){
-	board = std::vector<Circle>();
+	//init board and map and maxRadius
 
+}
+
+void Board::destroy(){
+	//destroy all
 }
 
 std::vector<int> Board::getSolution(){
 	return std::vector<int>();	
 }
 
-//std::vector<int> aStarSearch(std::vector<Circle> board, int start,int end){}
+
+inline bool Circle::operator==(const Circle& other){
+	return this->index == other.index;
+}
+	
+
+int getEdgeWeight(Circle* n1,Circle* n2){
+	if(*n1 == *n2){
+		return 0;
+	}
+	return 1;
+}
+
+float Board::getHeuristic(Circle* n1){
+	Circle* end = indToCircle.at(endCircle);
+	return (std::abs(n1->x - end->x)+std::abs(n1->y - end->y))/maxRadius;
+}
+
+
+
+
+std::vector<int> reconstruct_path(std::vector<int> cameFrom,int current){
+	std::vector<int> path;
+	while(current != -1){
+		path.push_back(current);
+		current = cameFrom.at(current);
+	}
+
+	return path; //TODO - reverse the order
+}
+
+
+std::vector<int> Board::aStarSearch(std::vector<Circle*> board, int start,int end){
+	std::set<int> closeSet; // The set of nodes already evaluated.
+
+	 // The set of tentative nodes to be evaluated, initially containing the start node
+	//std::priority_queue<int, std::vector<int>, std::greater<int>> openSetQueue;
+	std::set<int> openSet;
+	openSet.insert(start);
+	
+
+	std::vector<int> cameFrom; //a map of navigated nodes
+
+	float* g_score = new float (board.size());  // Cost from start along best known path.
+	g_score[start] = 0;
+	float* tenative_g_score = new float (board.size());  // Cost from start along best known path.
+	
+	//Estimated total cost from start to goal through y.
+	float* f_score = new float (board.size());  // Cost from start along best known path.
+	f_score[start] = g_score[start] + this->getHeuristic(indToCircle.at(start));
+	
+	while(openSet.size() > 0){
+		
+	
+		int minCircleIndex = std::min_element(openSet.begin,openSet.end);
+		Circle* currentCircle = indToCircle.at(minCircleIndex);
+		if(currentCircle == indToCircle.at(endCircle)){
+				delete[] g_score;
+				delete[] f_score;
+			return reconstruct_path(cameFrom, endCircle);
+		}
+
+
+		openSet.erase(currentCircle->index);
+		closeSet.insert(currentCircle->index);
+
+		for(std::vector<int>::iterator neighbour = currentCircle->neighbours.begin(); neighbour != currentCircle->neighbours.end(); ++neighbour) {
+			if(closeSet.count(currentCircle->index)!=0){
+				continue;
+			}
+
+			if(openSet.count(*neighbour)==0 || tenative_g_score[*neighbour] < g_score[*neighbour]){
+				cameFrom[*neighbour] = currentCircle->index;
+				g_score[*neighbour] = tenative_g_score[*neighbour];
+				f_score[*neighbour] = g_score[*neighbour] + getHeuristic(indToCircle.at(*neighbour));
+				if(openSet.count(*neighbour)==0){
+					openSet.insert(*neighbour);
+				}
+			}
+
+		}
+
+	}
+	
+	delete[] g_score;
+	delete[] f_score;
+	assert(0==1);
+
+	return std::vector<int>();
+	
+	
+}
+
--- aStarProject/Board/Board.h
+++ aStarProject/Board/Board.h
@@ -4,15 +4,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include <map>
+#define EPSILON 0.00001
 
 class Circle{
 
 public:
 	int x,y;
+	int index;
 	int radius;
 	std::vector<int> neighbours;
 	float R,G,B; //from 0 to 1
+
+	inline bool operator==(const Circle& other);
+	
 };
 
 
@@ -20,14 +25,22 @@ public:
 class Board{
 public:
 	void init(std::string boardPath,std::string imagePath);
-	std::vector<Circle> getBoard(){ return board;};	
+	void destroy();
+	std::vector<Circle*> getBoard(){ return board;};	
 	int getStartCircle(){ return startCircle; };
 	int getEndCircle(){ return endCircle; };
+	int getMaxRadius(){ return maxRadius; };
 	std::vector<int> getSolution();
 
+
+
 private:
+	float maxRadius;
 	int startCircle,endCircle;
-	std::vector<Circle> board;
+	std::vector<Circle*> board;
+	std::map<int,Circle*> indToCircle;
+	float getHeuristic(Circle* n1);
+	std::vector<int> aStarSearch(std::vector<Circle*> board, int start,int end);
 	
 };
 
